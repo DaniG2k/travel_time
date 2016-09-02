5801118c4fb0119942e0543436180c7189660c50
Daniele Pestilli <dpestilli@funnelback.com>
2016-09-02 14:59:01 +0100

    Fixed styling

--- app/models/route.rb
+++ app/models/route.rb
@@ -4,6 +4,18 @@ class Route < ApplicationRecord
   serialize :endcoords, Array
 
   def set_travel_time!
-    CityMapperJob.perform_later(id)
+    # Uncomment the following to enable Sidekiq job
+    # CityMapperJob.perform_later(id)
+    #
+    base_uri = 'https://developer.citymapper.com/api/1/traveltime/'
+    startcoord = "startcoord=#{startcoords.join('%2C')}"
+    endcoord = "endcoord=#{endcoords.join('%2C')}"
+    key = "key=#{Rails.application.secrets['citymapper_key']}"
+    time = "time=#{departs_at}"
+    str = "#{base_uri}?#{startcoord}&#{endcoord}&time_type=arrival&#{key}&#{time}"
+    
+    response = JSON.parse(HTTParty.get(str).body)
+    
+    self.update(travel_time: response['travel_time_minutes'])
   end
 end
--- spec/controllers/travel_times_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe TravelTimesController, type: :controller do
-
-end
