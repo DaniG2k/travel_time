3100a2cb6448ef46ef8d30d0b4d484b32dd69cdb
Daniele Pestilli <dpestilli@funnelback.com>
2016-09-02 15:37:39 +0100

    Added expected arrival time to route#show

--- spec/features/viewing_routes_spec.rb
+++ spec/features/viewing_routes_spec.rb
@@ -17,4 +17,13 @@ RSpec.feature 'Users can view travel times' do
 
     expect(page).to have_content 'Bzzz....Brrr....There was a problem!'
   end
+
+  scenario 'with arrival time estimate' do
+    visit '/'
+    fill_in 'Start address', with: '6-8 Long Lane, London EC1A 9HF'
+    fill_in 'End address', with: 'Victoria St, London SW1E 5ND'
+    click_button 'Calculate!'
+
+    expect(page).to have_content 'Your expected arrival time is'
+  end
 end
