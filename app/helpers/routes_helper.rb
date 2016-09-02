a541ae0fa4a93d1313d1c3051411f8fb5a4c9653
Daniele Pestilli <dpestilli@funnelback.com>
2016-09-02 23:30:00 +0100

    Added spec for checking routes index

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -4,19 +4,21 @@ module RoutesHelper
   end
 
   def full_itinerary
-    content_tag :ul do
-      time = @routes.first.departs_at.to_time
-      expected = time + @routes.first.travel_time.minutes
-      
-      @routes.each_with_index do |route, i|
-        if i > 0
-          concat content_tag(:li, "Wait 30 minutes...")
-          time = expected + 30.minutes
-          expected = time + route.travel_time.minutes
-        end
+    if @routes.present?
+      content_tag :ul do
+        time = @routes.first.departs_at.to_time
+        expected = time + @routes.first.travel_time.minutes
         
-        concat content_tag(:li, "Depart at #{time.strftime("%H:%M %P")} and arrive by #{expected.strftime("%H:%M %P")} (travel time is #{route.travel_time} mins)")
-      end
-    end.html_safe
+        @routes.each_with_index do |route, i|
+          if i > 0
+            concat content_tag(:li, "Wait 30 minutes...")
+            time = expected + 30.minutes
+            expected = time + route.travel_time.minutes
+          end
+          
+          concat content_tag(:li, "Depart at #{time.strftime("%H:%M %P")} and arrive by #{expected.strftime("%H:%M %P")} (travel time is #{route.travel_time} mins)")
+        end
+      end.html_safe
+    end
   end
 end
--- spec/features/viewing_index_spec.rb
+++ spec/features/viewing_index_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature 'Users can view all routes from the index' do
+  before do
+    visit '/'
+    fill_in 'Start address', with: '6-8 Long Lane, London EC1A 9HF'
+    fill_in 'End address', with: 'Euston Rd, London N1 9AL'
+    click_button 'Calculate!'
+  end
   scenario 'successfully' do
     visit '/'
     click_link 'All routes'
 
     expect(page).to have_content('Depart at')
-    expect(page).to have_content('Arrive by')
+    expect(page).to have_content('and arrive by')
   end
 end
